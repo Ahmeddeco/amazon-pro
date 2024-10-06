83536e31c3150bed007c6b13f0ddcd18c500d98c
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-06 19:03:43 +0300

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 import { cn } from '@/lib/utils'
 import 'slick-carousel/slick/slick.css'
+import { Analytics } from '@vercel/analytics/react'
 
 import 'slick-carousel/slick/slick-theme.css'
 import Footer from '@/components/Footer'
@@ -33,6 +34,7 @@ export default function RootLayout({
 			>
 				<Navbar />
 				{children}
+				<Analytics />
 				<Footer />
 			</body>
 		</html>
